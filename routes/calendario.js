f396718af8eda697a87ed0afc92ebdc7f7763dc4
Luixls <luisaraujo1998@gmail.com>
2024-03-10 18:27:21 -0400

    limintar CRUD en calendario (trimestres)
    Co-Authored-By: denilsoncastellanos <109046755+denilsoncastellanos@users.noreply.github.com>

--- routes/calendario.js
+++ routes/calendario.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const CalendarioController = require("../controllers/CalendarioController");
+const { verificarTokenYRol } = require("../middlewares/authMiddleware");
 
+// No se requiere autenticación
 router.get("/listar", CalendarioController.listar);
 router.get(
   "/actividades/:trimestre/:semana",
   CalendarioController.actividadesSemana
 );
-router.post("/agregar", CalendarioController.agregar);
-router.put("/editar/:id", CalendarioController.editar);
-router.delete("/eliminar/:id", CalendarioController.eliminar);
+
+// Solo el director puede agregar trimestres
+router.post(
+  "/agregar",
+  verificarTokenYRol(["Director"]),
+  CalendarioController.agregar
+);
+
+// Solo el director puede editar trimestres
+router.put(
+  "/editar/:id",
+  verificarTokenYRol(["Director"]),
+  CalendarioController.editar
+);
+
+// Solo el director puede eliminar
+router.delete(
+  "/eliminar/:id",
+  verificarTokenYRol(["Director"]),
+  CalendarioController.eliminar
+);
 
 module.exports = router;

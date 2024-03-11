2445c74d766c2dc74570a93dc06f6c68cfa9ed1c
Luixls <luisaraujo1998@gmail.com>
2024-03-10 22:11:24 -0400

    quitar validación de usuario en registrar
    cómo puedes crear una cuenta si necesitas tener una cuenta para crear una cuenta?

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const UsuarioController = require("../controllers/UsuarioController");
-const { verificarTokenYRol } = require("../middlewares/authMiddleware");
 const {
   validarRegistroUsuario,
   validarUsuario,
 } = require("../middlewares/validacionMiddleware");
 
 // Solo el director puede agregar usuarios
-router.post(
-  "/registro",
-  validarRegistroUsuario,
-  verificarTokenYRol(["Director"]),
-  UsuarioController.registro
-);
+router.post("/registro", validarRegistroUsuario, UsuarioController.registro);
 
 router.post("/login", validarUsuario, UsuarioController.login);
 
